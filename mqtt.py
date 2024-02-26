a841cc4b2f36d4fba5dcb818789d4b4957cb4a39
Smart309 <pattharapon_takham@cmu.ac.th>
2024-02-26 12:11:00 +0700

    "Verify payload from mqtt about emergency and remote"

--- mqtt.py
+++ mqtt.py
@@ -1,10 +1,35 @@
-import time
 import paho.mqtt.client as paho
+import RPi.GPIO as GPIO
 from paho import mqtt
 import getmac
-import os 
+import time
+import os
+import threading
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+BUZZER = 18
+GPIO.setup(BUZZER, GPIO.OUT)
 
 mac = getmac.get_mac_address("eth0")
+
+emgc = 0
+
+def buzz(noteFreq, duration):
+    halveWaveTime = 1 / (noteFreq * 2 )
+    waves = int(duration * noteFreq)
+    for i in range(waves):
+       GPIO.output(BUZZER, True)
+       time.sleep(halveWaveTime)
+       GPIO.output(BUZZER, False)
+       time.sleep(halveWaveTime)
+def play():
+    t=0
+    x=1.5
+    duration=[0.25,0.25,0.25,0.5,0.5,0.5,0.25,0.25,0.25]
+    for d in duration:
+        buzz(500, d/x)
+        time.sleep((d/x) *0.1)
 
 # setting callbacks for different events to see if it works, print the message etc.
 def on_connect(client, userdata, flags, rc, properties=None):
@@ -20,11 +45,26 @@ def on_subscribe(client, userdata, mid, granted_qos, properties=None):
 
 # print message, useful for checking if it was successful
 def on_message(client, userdata, msg):
+    global emgc
     print(msg.topic + " " + 'message =>' + " " + str(msg.payload))
-    temp = 'b' + '\''+mac+'\''
-    if str(msg.payload) == temp:
-       print(True)
-       os.system("irsend SEND_ONCE LG KEY_POWER")
+    if "Not EMGC" in str(msg.payload):
+        if mac in str(msg.payload):
+            print("Not EMGC")
+            emgc = 0
+    elif "EMGC" in str(msg.payload):
+        if mac in str(msg.payload):
+            print("EMGC")
+            emgc = 1
+    else:
+        if mac in str(msg.payload):
+            print("Remote")
+            os.system("irsend SEND_ONCE LG KEY_POWER")
+
+def check_emgc():
+    while True:
+        if emgc == 1:
+            play()
+        time.sleep(1)  # Adjust sleep time as needed
 
 # using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
 # userdata is user defined data of any type, updated by user_data_set()
@@ -53,4 +93,12 @@ client.subscribe("raspberrypi", qos=0)
 
 # loop_forever for simplicity, here you need to stop the loop manually
 # you can also use loop_start and loop_stop
-client.loop_forever()
+#client.loop_forever()
+
+# Start MQTT client loop in a separate thread
+client_thread = threading.Thread(target=client.loop_forever)
+client_thread.daemon = True
+client_thread.start()
+
+# Run emgc check in the main thread
+check_emgc()
